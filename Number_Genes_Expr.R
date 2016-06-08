34412be977d173c3634d2c2959b482d728608514
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2016-06-07 19:08:00 -0700

    Change to CPM

--- Number_Genes_Expr.R
+++ Number_Genes_Expr.R
@@ -6,18 +6,31 @@ rm(list=ls())
 sessionInfo()
 
 # Input gene expression table (counts from HTseq)
-exDatDF <- read.csv("../data/HTSC/Exprs_HTSCexon.csv", header = TRUE, row.names = 1)
+exDatDF <- read.csv("../data/htseq/Exprs_HTSCexon.csv", header = TRUE, row.names = 1)
 dim(exDatDF)
 
-# Number of genes expressed
+# Picard Sequencing Statistics - scRNAseq
+picStatsDF <- read.csv("../metadata/PicardToolsQC.csv")
+
+# Calculate CPM
+exDatDF <- exDatDF[ ,order(names(exDatDF))]
+cpmDF <- exDatDF / (picStatsDF$PF_READS_ALIGNED / 10^6)
+
+# Number of genes expressed - raw counts
 gExprd <- apply(exDatDF, 2, function(genes) length(subset(genes, genes >= 1)))
 mgExprd <- mean(gExprd)
 mean(gExprd)
 median(gExprd)
-pdf("../analysis/graphs/Histogram_Genes_Expressed.pdf")
-png("../analysis/graphs/Histogram_Genes_Expressed.png")
 hist(gExprd, breaks = 20, xlab = "Genes Detected with >=1 reads"
 	, main = paste("Number of Genes Expressed", "\nMean: ", mgExprd, sep=""))
 dev.off()
 
-
+# Number of genes expressed - CPM
+gExprd <- apply(cpmDF, 2, function(genes) length(subset(genes, genes >= 1)))
+mgExprd <- mean(gExprd)
+mean(gExprd)
+median(gExprd)
+pdf("../analysis/graphs/Number_Genes_Detected_Histogram.pdf")
+hist(gExprd, breaks = 20, col = "blue", xlab = "Number of genes with >=1 CPM"
+	, main = paste("Number of Genes Detected Per Cell", "\nMean: ", round(mgExprd,2), sep=""))
+dev.off()
